102f75341a2fd3a07eac11d6ce5ff3b5e3ba9289
Cylas Kiganda <kigandacylas@gmail.com>
2024-02-08 14:01:40 +0300

    sticky header, grid tweaks

--- belo-pack/assets/js/belo-pack.js
+++ belo-pack/assets/js/belo-pack.js
@@ -1,19 +1,6 @@
 (function ($) {
   "use strict";
 
-  // Header Type = Fixed
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    var box = $(".header-text").height();
-    var header = $("header").height();
-
-    if (scroll >= box - header) {
-      $("header").addClass("background-header");
-    } else {
-      $("header").removeClass("background-header");
-    }
-  });
-
   $(".owl-our-team").owlCarousel({
     items: 3,
     loop: true,
